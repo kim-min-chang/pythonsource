0cad74a42bef1bd754573bf8fd9fa8386ddf0926
김민창 <rlaalsckd5064@gmail.com>
2025-01-03 17:47:22 +0900

    python

---
+++ basic/mod3.py
@@ -0,0 +1,14 @@
+PI = 3.141592
+
+class Math:
+    def solv(self,r):
+        return PI * (r ** 2)
+    
+def add(self,a,b):
+        return a + b
+    
+if __name__ == "__main__":
+    print(PI)
+    math = Math()
+    print(math.add(5,8))
+    print(math.solv(5))
